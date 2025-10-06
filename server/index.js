ba13e4868c1afde5e3b7c1445eb98806687d2a4f
Evgeny Kirichuk <zhenyakirichuk@gmail.com>
2025-10-06 12:30:51 +0200

    init

---
+++ server/index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+app.get("/api/hello", (req, res) => {
+	res.json({ message: "Hello from API!" });
+});
+
+app.listen(port, () => {
+	console.log(`Server running at http://localhost:${port}`);
+});
